36e1c22657eae156db494424892616c965ffd31a
Eric Dietrich <eric.dietrich@alpes.one>
2021-11-03 18:52:07 -0300

    add default task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var htmlReplace = require('gulp-html-replace');
 
+gulp.task('default', ['copy'], function () {
+  /* as 3 funções abaixo não tem retorno, então elas são executadas assincronamente */
+  /* uma não depende da outra */
+  gulp.start('build-img', 'build-js', 'build-html')
+})
+
 //copia tudo da pasta 'src' para a pasta 'dist'
 gulp.task('copy', ['clean'] ,function () {
   //se não houver o return, as tasks 'copy' e 'build-img' rodaram em simultaneo, contudo 'build-img 'só pode rodar depois de 'copy' finalizado
@@ -18,21 +24,22 @@ gulp.task('clean', function () {
 })
 
 //minifica as imagens da pasta 'img'
-gulp.task('build-img', ['copy'], function () {
-  return gulp.src('dist/img/**/*')
+gulp.task('build-img', function () {
+  gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'))
 })
 
 /* concatena todos os arquivos compilados js num único arquivo */
-gulp.task('build-js', ['build-img'], function () {
-  return gulp.src('dist/js/**/*.js')
+/* os arquivos são listado para a importação acontecer nessa ordem */
+gulp.task('build-js', function () {
+  gulp.src(['dist/js/jquery.js', 'dist/js/print-a.js', 'dist/js/print-b.js'])
     .pipe(concat('all.js'))
     .pipe(gulp.dest('dist/js'))
 })
 
 /* substitui a importação de todos os arquivos js no html pelo concatenado */
-gulp.task('build-html', ['build-js'], function() {
+gulp.task('build-html',  function() {
   gulp.src('dist/**/*.html')
     .pipe(htmlReplace({
       js: 'js/all.js'
