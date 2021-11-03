da005ba7f9c6a490da8953739b5649c30bce6931
Eric Dietrich <eric.dietrich@alpes.one>
2021-11-03 18:37:18 -0300

    add js concat and html replacer

---
+++ dist/js/all.js
@@ -0,0 +1,6 @@
+for(let i = 0; i < 10; i++) {
+  console.log(`print-a ${i + 1}`);
+}
+for(let i = 0; i < 10; i++) {
+  console.log(`print-b ${i + 1}`);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,41 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var htmlReplace = require('gulp-html-replace');
 
-gulp.task('build-img', function(){
-  gulp.src('src/img/**/*')
+//copia tudo da pasta 'src' para a pasta 'dist'
+gulp.task('copy', ['clean'] ,function () {
+  //se não houver o return, as tasks 'copy' e 'build-img' rodaram em simultaneo, contudo 'build-img 'só pode rodar depois de 'copy' finalizado
+  return gulp.src('src/**/*')
+    .pipe(gulp.dest('dist'))
+})
+
+//exclui a pasta 'dist'
+gulp.task('clean', function () {
+  //se não houver o return, as tasks 'copy' e 'clean' rodaram em simultaneo, contudo 'copy 'só pode rodar depois de 'clean' finalizado
+  return gulp.src('dist').pipe(clean())
+})
+
+//minifica as imagens da pasta 'img'
+gulp.task('build-img', ['copy'], function () {
+  return gulp.src('dist/img/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('src/img'))
+    .pipe(gulp.dest('dist/img'))
+})
+
+/* concatena todos os arquivos compilados js num único arquivo */
+gulp.task('build-js', ['build-img'], function () {
+  return gulp.src('dist/js/**/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/js'))
+})
+
+/* substitui a importação de todos os arquivos js no html pelo concatenado */
+gulp.task('build-html', ['build-js'], function() {
+  gulp.src('dist/**/*.html')
+    .pipe(htmlReplace({
+      js: 'js/all.js'
+    }))
+    .pipe(gulp.dest('dist'))
 })
